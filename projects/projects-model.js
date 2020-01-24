6b29b088e9ca344f42147cb55de795dac1a65bf0
maxjamb <maxwelljamesbeard@gmail.com>
2020-01-24 11:09:31 +0000

    added projects, resources, tasks (model/router)

---
+++ projects/projects-model.js
@@ -0,0 +1,38 @@
+const db = require("../data/db-config");
+
+const getProjects = () => {
+  return db("project");
+};
+
+const getProject = id => {
+  return db("project")
+    .where({ id })
+    .then(projects => projects[0]);
+};
+
+const add = project => {
+  return db("project")
+    .insert(project)
+    .then(ids => getResource(ids[0]));
+};
+
+const update = (changes, id) => {
+  return db("project")
+    .where({ id })
+    .update(changes)
+    .then(() => getProject(id));
+};
+
+const remove = id => {
+  return db("project")
+    .where({ id })
+    .del();
+};
+
+module.exports = {
+  getProjects,
+  getProject,
+  add,
+  update,
+  remove
+};
